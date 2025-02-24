f5a63920806d2c81a7508748d1b0126a3d8029ba
Pongsakon <charlee2543@hotmail.com>
2025-02-24 16:10:21 +0700

    feat:charlee add ex1-ex3

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,11 @@
 let isOver18;
 let hasCriminalBlacklist;
-
 let isAllow;
 
 // Start coding here
+isAllow = isOver18 > 18 && !hasCriminalBlacklist;
+//This James
+isOver18 = 18;
+hasCriminalBlacklist = false;
 
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -8,5 +8,17 @@ let hasAttendedDiscountEvent;
 let isPlatinum;
 
 let hasPromotion;
+hasPromotion =
+	(lastMonthPaidMoreThan4000 > 4000 &&
+		!(isWeekday == "Saturday", isWeekday == "Sunday") &&
+		!hasBoughtProductFromITCategory &&
+		!hasAttendedDiscountEvent) ||
+	isPlatinum == "Platinum";
+//John
+lastMonthPaidMoreThan4000 = 4001;
+isWeekday = "friday";
+hasBoughtProductFromITCategory = false;
+hasAttendedDiscountEvent = true;
+isPlatinum = "Gold";
 
 console.log(hasPromotion);
